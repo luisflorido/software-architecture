e30899b6f6dfab2386b1c57cafad6683166c5e07
lukasbp <44759049+lukasbp@users.noreply.github.com>
2020-03-23 00:27:59 -0300

    final commit

--- list/src/main/java/com/br/students/list/App.java
+++ list/src/main/java/com/br/students/list/App.java
@@ -32,40 +32,74 @@ public class App {
     private static String clearString(String str){
         return str.toLowerCase().replaceAll(" ", "");
     }
+    
+    private static void criarAluno() {
+    	System.out.println("Digite o nome do aluno: ");
 
+        String name = scanner.nextLine();
+        Optional<String> aluno = arlist.stream().filter(e -> clearString(e).equals(clearString(name))).findAny();
+        if (!aluno.isPresent()) {
+            arlist.add(name);
+        } else {
+            System.out.println("O aluno já esta cadastrado!");
+            criarAluno();
+        }
+    }
+    
+    private static void listarAlunos() {
+    	System.out.println("Listando ...");
+        /*
+            Method reference
+            poderia ser utilizado também com lambda
+            arlist.forEach(e -> System.out.println(e));
+            for ou for each padrão
+         */
+        arlist.forEach(System.out::println);
+        System.out.println("");
+    }
+    
+    private static void exibirMenu() {
+    	System.out.println("Menu");
+        System.out.println("Digite 1 para inserir o estudante ");
+        System.out.println("Digite 2 para imprimir a lista ");
+        System.out.println("Digite 3 sair \n");
+
+    }
+    
     private static void carregarMenu() {
         while (run) {
-            System.out.println("Menu");
-            System.out.println("Digite 1 para inserir o estudante ");
-            System.out.println("Digite 2 para imprimir a lista ");
-            System.out.println("Digite 3 sair \n");
-
+        	exibirMenu();
             String option = scanner.nextLine();
 
             if (option.equals("1")) {
-                System.out.println("Digite o nome do aluno: ");
-
-                String name = scanner.nextLine();
-                Optional<String> aluno = arlist.stream().filter(e -> clearString(e).equals(clearString(name))).findAny();
-                if (!aluno.isPresent()) {
-                    arlist.add(name);
-                } else {
-                    System.out.println("O aluno já esta cadastrado!");
-                }
+            	criarAluno();
+            	System.out.println("Aluno Cadastrado");
+//                System.out.println("Digite o nome do aluno: ");
+//
+//                String name = scanner.nextLine();
+//                Optional<String> aluno = arlist.stream().filter(e -> clearString(e).equals(clearString(name))).findAny();
+//                if (!aluno.isPresent()) {
+//                    arlist.add(name);
+//                } else {
+//                    System.out.println("O aluno já esta cadastrado!");
+//                }
+            	
             } else if (option.equals("2")) {
-                System.out.println("Listando ...");
-                /*
-                    Method reference
-                    poderia ser utilizado também com lambda
-                    arlist.forEach(e -> System.out.println(e));
-                    for ou for each padrão
-                 */
-                arlist.forEach(System.out::println);
-                System.out.println("");
+//            	System.out.println("Listando ...");
+//                /*
+//                    Method reference
+//                    poderia ser utilizado também com lambda
+//                    arlist.forEach(e -> System.out.println(e));
+//                    for ou for each padrão
+//                 */
+//                arlist.forEach(System.out::println);
+//                System.out.println("");
+            	listarAlunos();
             } else if (option.equals("3")) {
                 run = false;
             } else {
                 System.out.println("Digite uma opção válida");
+                exibirMenu();
             }
         }
     }
